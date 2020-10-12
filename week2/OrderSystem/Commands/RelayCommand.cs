6334f67f8ef639fccec4bc9aa1df8229fedf65e5
omonimus <davidepollicino2015@gmail.com>
2020-10-12 20:14:31 +0100

    Creates views project week2 OrderSystem

---
+++ week2/OrderSystem/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OrderSystem.Commands
+{
+    class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public event EventHandler CanExecuteChanged = (Sender, e) => { };
+
+        PublishLicense RelayCommnd(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true; 
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ week2/OrderSystem/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using OrderSystem.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OrderSystem.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        public string AddOrderButtonContent { get; private set; }
+        public string ViewOrderButtonContent { get; private set; }
+
+        public ICommand AddOrderButtonCommand { get; private set; }
+        public ICommand ViewOrderButtonCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            AddOrderButtonContent = "Add order";
+            ViewOrderButtonCommand = "View Orders";
+
+            AddOrderButtonCommand = new RelayCommand(AddOrderButtonClick);
+            ViewOrderButtonCommand = new RelayCommand(ViewOrderButtonClick); 
+        }
+
+        private void AddOrderButtonClick()
+        {
+
+        }
+
+        private void ViewOrderButtonClick()
+        {
+
+        }
+    }
+}
